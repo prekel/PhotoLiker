33d8d0fbef448110a4eba70971b07e837a21a967
Vladislav Prekel <misterptits@yandex.ru>
2018-12-31 18:36:17 +0700

    fix #deploy

--- scripts/publish.ps1
+++ scripts/publish.ps1
@@ -6,7 +6,7 @@ dotnet publish -c Release /p:AssemblyVersion="$env:APPVEYOR_BUILD_VERSION"
 #	$temp = "./publish/MyMessenger_Client_$TAG" + "_linux-x64"
 #	dotnet publish -c Release -r linux-x64 -p:AssemblyVersion="$env:APPVEYOR_BUILD_VERSION" -o "$temp" -p:Version="$version" MyMessenger.Client.Console/MyMessenger.Client.Console.csproj
 #	$temp = "./publish/MyMessenger_Client_$TAG" + "_win-x64"
-3	dotnet publish -c Release -r win-x64 -p:AssemblyVersion="$env:APPVEYOR_BUILD_VERSION" -o "$temp" -p:Version="$version" MyMessenger.Client.Console/MyMessenger.Client.Console.csproj
+#	dotnet publish -c Release -r win-x64 -p:AssemblyVersion="$env:APPVEYOR_BUILD_VERSION" -o "$temp" -p:Version="$version" MyMessenger.Client.Console/MyMessenger.Client.Console.csproj
 #	$temp = "./publish/MyMessenger_Client_$TAG" + "_win-x86"
 #	dotnet publish -c Release -r win-x86 -p:AssemblyVersion="$env:APPVEYOR_BUILD_VERSION" -o "$temp" -p:Version="$version" MyMessenger.Client.Console/MyMessenger.Client.Console.csproj
 #	$temp = "./publish/MyMessenger_Server_$TAG" + "_linux-x64"
